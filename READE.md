78332ad2e6388e7b70e349bc48a408dd4a03ac36
xTCry [Vladislav Kh] <wolt6@mail.ru>
2020-08-15 17:35:32 +0300

    Update

---
+++ READE.md
@@ -0,0 +1,21 @@
+# Maditoy
+VK Bot для сохранения видео с YouTube в VK
+
+## Config
+Переименовать файл `.env.example` в `.env` и настроить его
+
+## Install
+```sh
+yarn
+# or
+npm i
+```
+
+## Run
+```sh
+yarn run
+# or
+npm run run
+# or
+node .
+```
--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-
 const fs = require('fs');
 const { VK } = require('vk-io');
 const youtubedl = require('youtube-dl');
 require('dotenv').config();
 
-const vkLP = new VK({
-    token: process.env.TOKEN,
-});
-const vkUploader = vkLP;
+const withoutVideoSave = process.env.VIDEO_SAVE == 'false';
 
-vkLP.updates
-    .hear(/^md (?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/i, async (context) => {
-        let math = context.text.match(/(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>\s]{0,11})/i);
+const vkLongPool = new VK({
+    token: process.env.TOKEN_LP,
+});
+const vkUploader = process.env.TOKEN_UP && new VK({
+    token: process.env.TOKEN_UP,
+}) || vkLongPool;
+
+const regExpYT = new RegExp('(?:http(?:s)?:\/\/)?(?:www\\.)?(?:m\\.)?(?:youtu\\.be\/|youtube\\.com\/(?:(?:watch)?\\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([\\w\\d]{0,11})', 'i');
+const regExpVK = new RegExp(`^md ${regExpYT.toString().slice(1, -2)}`, 'i');
+
+vkLongPool.updates
+    .hear(regExpVK, async (context) => {
+        let math = context.text.match(regExpYT);
         if (math.length > 1 && math[1]) {
             let videoID = math[1];
             context.send(`Пошла загрузочка...`);
+
             try {
                 let videoAttachment = await GO(`http://www.youtube.com/watch?v=${videoID}`, true);
                 context.send({
@@ -28,6 +35,11 @@ vkLP.updates
         }
     })
     .start()
+    .then(() => {
+        const isGroup = !!vkLongPool.options.pollingGroupId;
+
+        console.log('isGroup', isGroup);
+    })
     .catch(console.error);
 
 
@@ -38,12 +50,17 @@ const GO = async (url, upload = false, peer = null) => {
     // console.log('INFO: ', info);
 
     let output = `${__dirname}/videos/${info.id}.mp4`;
-    let downloaded = fs.existsSync(output) ? fs.statSync(output).size : 0;
-    if (!downloaded || downloaded >= info.size) {
-        await downloadMe(info, output);
+    if (withoutVideoSave && upload) {
+        output = await downloadMe(info);
     }
     else {
-        console.log(`Video [${info.id}] "${info.title}" already downloaded.`);
+        let downloaded = fs.existsSync(output) ? fs.statSync(output).size : 0;
+        if (!downloaded || downloaded >= info.size) {
+            await downloadMe(info, output);
+        }
+        else {
+            console.log(`Video [${info.id}] "${info.title}" already downloaded.`);
+        }
     }
 
     if (upload) {
@@ -58,8 +75,8 @@ const getInfo = async (url) => new Promise((resolve, reject) => youtubedl.getInf
     if (err) reject(err); else resolve(info);
 }));
 
-const downloadMe = async (data, output) => new Promise((resolve, reject) => {
-    let downloaded = fs.existsSync(output) ? fs.statSync(output).size : 0;
+const downloadMe = async (data, output = false) => new Promise((resolve, reject) => {
+    let downloaded = output && fs.existsSync(output) ? fs.statSync(output).size : 0;
 
     const video = youtubedl(data, [], { start: downloaded, cwd: __dirname });
     let resultInfo = {};
@@ -85,24 +102,31 @@ const downloadMe = async (data, output) => new Promise((resolve, reject) => {
         resolve(resultInfo);
     });
 
-    video.pipe(fs.createWriteStream(output, { flags: 'a' }));
+    if (output) {
+        video.pipe(fs.createWriteStream(output, { flags: 'a' }));
+    }
+    else {
+        resolve(video);
+    }
 });
 
-const uploadVK = async (path, name = Date.now(), description = undefined, peer_id = null) => {
+const uploadVK = async (output, name = Date.now(), description = undefined, peer_id = null) => {
+    const sourceValues = [typeof output === 'string' ? fs.createReadStream(output) : output];
+
     let videoAttachment = await vkUploader.upload.video({
         name,
         description,
         is_private: 1,
-		source: {
+        source: {
             timeout: 60e3 * 5,
-            values: [fs.createReadStream(path)]
+            values: sourceValues,
         },
     });
     console.log('Video uploaded', videoAttachment);
 
     // Send message att video
     if (peer_id) {
-        vkUploader.api.messages.send({
+        vkLongPool.api.messages.send({
             attachment: videoAttachment,
             message: `Видео. ${description}`,
             peer_id,
@@ -112,7 +136,8 @@ const uploadVK = async (path, name = Date.now(), description = undefined, peer_i
     return videoAttachment;
 };
 
-const genDesc = (id) => `[By xTCry] Видео скачано с (https://youtu.be/${id})`;
+const wm = '​﻿​﻿​﻿‌﻿​﻿‌﻿‌﻿‍﻿‏﻿​﻿​﻿​﻿​﻿‍﻿‌﻿‍﻿‌﻿‏﻿​﻿​﻿​﻿‌﻿‌﻿‌﻿‍﻿‍﻿‏﻿​﻿​﻿​﻿​﻿‌﻿‌﻿​﻿​﻿‏﻿​﻿​﻿​﻿‌﻿‌﻿‌﻿‍﻿‌﻿‏﻿​﻿​﻿​﻿‌﻿​﻿​﻿‍﻿‌﻿‏﻿​﻿​﻿​﻿​﻿‍﻿‌﻿‍﻿‍﻿‏﻿​﻿​﻿​﻿‌﻿‌﻿‌﻿​﻿‌﻿‏﻿​﻿​﻿​﻿‌﻿‌﻿‌﻿‍﻿‍﻿‏﻿​﻿​﻿​﻿‌﻿​﻿‌﻿‍﻿‌';
+const genDesc = (id) => `Скачано​‌${wm} с (https://youtu.be/${id})`;
 
 
 /* INITIALIZE */
